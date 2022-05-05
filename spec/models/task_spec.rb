b69a793997d21d3b1703d800b35f631198751180
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-05 15:37:10 +0800

    added  tasks

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:task) { Task.new }
+
+  context 'validation' do
+    it '- is not valid without a name' do
+      task.name = nil
+
+      expect(task).to_not be_valid
+    end
+  end
 end
