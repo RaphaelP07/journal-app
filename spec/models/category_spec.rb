996040681e7906be46be06fa211751b06e826499
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-05 09:37:43 +0800

    added category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  
+  let!(:category) { Category.new }
+
+  context 'Validations' do
+    it '1. is not valid without title' do
+      category.description = 'Sample description'
+      category.title = nil
+
+      expect(category).to_not be_valid
+      # expect(category.errors).to be_present
+      # expect(category.errors.to_h.keys).to include(:title)
+    end
+
+    it '2. should have a title with max length of 18' do
+      category.title = 'qwerqwerqwerqwerqwer' # 20 chars
+
+      expect(category).to_not be_valid
+    end
+
+    it '3. should have a unique title' do
+      Category.create(title: "Breaking Dawn")
+
+      category.title = "Breaking Dawn"
+
+      expect(category).to_not be_valid
+    end
+
+    it '4. should create a valid category' do
+      category.title = "Breaking Dawn"
+
+      expect(category).to be_valid
+    end
+  end
+end
